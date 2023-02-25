88380b57400fc2b4df2a5bec6430bbdd63cda570
Aritesoma <99509115+Aritesoma@users.noreply.github.com>
2023-02-25 14:55:17 +0100

    header

--- 0x05-react_state/task_2/dashboard/src/Header/Header.js
+++ 0x05-react_state/task_2/dashboard/src/Header/Header.js
@@ -1,31 +1,49 @@
-import React from 'react';
-import { StyleSheet, css } from 'aphrodite';
-import logo from '../assets/hbnblogo.jpg';
+import React, { useContext } from "react";
+import logo from "../assets/holberton-logo.jpg";
+import { StyleSheet, css } from "aphrodite";
+import { AppContext } from "../App/AppContext";
 
-const styles = StyleSheet.create({
-  header: {
-    display: "flex",
-    alignItems: "center",
-    borderBottom: "4px solid #E0354B",
-    marginBottom: 60
-  },
-  
-  img: {
-    width: 160,
-    height: 160
-  },
-  
-  heading: {
-    color: "#E0354B"
-  }
-})
+function Header() {
+  const { user, logOut } = useContext(AppContext);
 
-export default function Header() {
   return (
-    <div className={css(styles.header)}>
-      <img className={css(styles.img)} src={logo} alt="logo"/>
-      <h1 className={css(styles.heading)}>School dashboard</h1>
-    </div>
-  )
+    <>
+      <div className={css(styles["App-header"])}>
+        <img src={logo} className={css(styles.img)} alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+
+      {user.isLoggedIn && (
+        <section className={css(styles.greeting)} id="logoutSection">
+          Welcome<strong> {user.email} </strong>
+          <em>
+            <a href="#" onClick={logOut}>
+              (logout)
+            </a>
+          </em>
+        </section>
+      )}
+    </>
+  );
 }
 
+const styles = StyleSheet.create({
+  "App-header": {
+    fontSize: "1.4rem",
+    color: "#e0354b",
+    display: "flex",
+    alignItems: "center",
+    borderBottom: "3px solid #e0354b",
+  },
+
+  img: {
+    width: "200px",
+    height: "200px",
+  },
+
+  greeting: {
+    marginTop: "1rem",
+  },
+});
+
+export default Header;
