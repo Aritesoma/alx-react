532c42676c18a808948fb416ea2706b2292794ba
Aritesoma <99509115+Aritesoma@users.noreply.github.com>
2023-03-08 09:56:18 -0800

    footer

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
@@ -1,33 +1,31 @@
-import React from 'react';
-import { StyleSheet, css} from 'aphrodite';
-import { getFullYear, getFooterCopy } from '../utils/utils'
+import React from "react";
+import { connect } from "react-redux";
+import { getFullYear, getFooterCopy } from "../utils/utils";
+import PropTypes from "prop-types";
 
-const styles = StyleSheet.create({
-  AppFooter: {
-    borderTop: "4px solid #E0354B",
-    position: "absolute",
-    bottom: '0%',
-    width: '99%',
-    '@media (max-width: 900px)': {
-      position: "relative",
-      width: "100%",
-      marginTop: 60
-    }
-  },
-  
-  AppFooter_p: {
-    fontStyle: "italic",
-    textAlign: "center"
-  }
-
-
-})
-
-export default function Footer() {
+export function Footer({ user }) {
   return (
-    <div className={css(styles.AppFooter)}>
-      <p className={css(styles.AppFooter_p)}>Copyright {getFullYear()} - {getFooterCopy(false)}</p>
+    <div className="footer">
+      <p>
+        Copyright {getFullYear()} - {getFooterCopy(true)}
+      </p>
+      {user && <a href="#">Contact us</a>}
     </div>
-    )
-  }
+  );
+}
 
+Footer.defaultProps = {
+  user: null,
+};
+
+Footer.propTypes = {
+  user: PropTypes.object,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.get("user"),
+  };
+};
+
+export default connect(mapStateToProps, null)(Footer);
